40d0c1ce8bf77d0aefac8fb51d9430184c1fe796
casheww <51793722+casheww@users.noreply.github.com>
2021-12-07 09:02:11 +0000

    2021 day 7

---
+++ 2021/07-treachery-of-whales/crabs.r
@@ -0,0 +1,15 @@
+
+crabs <- unlist(read.csv("07-treachery-of-whales/input.txt", header=FALSE))
+
+# part 1 : 1 fuel per move
+
+avg <- round(median(crabs))
+sum(abs(crabs - avg))
+
+# part 2 : increasing fuel per move
+avg <- floor(mean(crabs))
+dists <- abs(crabs - avg)
+avg
+dists
+dists * (dists + 1) / 2
+sum(dists * (dists + 1) / 2)
