68e3d4474b377e2ac434491f1c552b028b89cb40
casheww <51793722+casheww@users.noreply.github.com>
2022-12-06 19:46:05 +0000

    2022 day 6

---
+++ 2022/d6-tuning/tuning.c
@@ -0,0 +1,41 @@
+#include "../helper.h"
+
+#define START_OF_PACKAGE_LEN  4
+#define START_OF_MESSAGE_LEN 14
+
+int find_unique_char_str(char *str, int len, int targetLen);
+int has_duplicate_chars(char *str, int len);
+
+int main() {
+    int charCount;
+    char *input = fileRead("input.txt", &charCount);
+
+    int sop_start = find_unique_char_str(input, charCount, 4);
+    int som_start = find_unique_char_str(input, charCount, 14);
+
+    printf("part 1: %d\npart 2: %d\n", sop_start, som_start);
+}
+
+
+int find_unique_char_str(char *str, int len, int targetLen) {
+    char trial[targetLen];
+
+    for (int i = 0; i < len - targetLen; i++) {
+        for (int j = 0; j < targetLen; j++)
+            trial[j] = str[i+j];
+
+        if (!has_duplicate_chars(trial, targetLen))
+            return i + targetLen;
+    }
+
+    return -1;      // no result
+}
+
+int has_duplicate_chars(char *str, int len) {
+    for (int i = 0; i < len; i++)
+    for (int j = 0; j < len; j++)
+        if (i != j && str[i] == str[j])
+            return 1;
+
+    return 0;
+}
