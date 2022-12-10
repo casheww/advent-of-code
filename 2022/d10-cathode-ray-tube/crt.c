ca740050355fdcf4dad0e06c18c2e9791212d1db
casheww <51793722+casheww@users.noreply.github.com>
2022-12-10 14:13:54 +0000

    2022 day 10

---
+++ 2022/d10-cathode-ray-tube/crt.c
@@ -0,0 +1,48 @@
+#include "../helper.h"
+
+#define CRT_WIDTH  40
+#define CRT_HEIGHT  6
+
+int main() {
+    int _, line_count;
+    char **lines = split(file_read("input.txt", &_), "\n", &line_count);
+
+    int i=0, cycle=1, wait=0;
+    int X=1, buffer=0;
+
+    int part1_strength_sum = 0;
+    int test_cycles[6] = {20, 60, 100, 140, 180, 220};
+
+    for (; i < line_count || wait; cycle++) {
+        for (int j = 0; j < 6; j++) {
+            if (cycle == test_cycles[j]) {
+                part1_strength_sum += test_cycles[j] * X;
+            }
+        }
+
+        int crt_x = (cycle-1) % CRT_WIDTH;
+        if (!crt_x)
+            printf("\n");
+
+        if (ABS(crt_x - X) <= 1)
+            printf("#");
+        else
+            printf(".");
+
+        if (wait) {
+            wait = 0;
+            X += buffer;
+        } else {
+            if (strcmp(lines[i], "noop")) {
+                char **instr = split(lines[i], " ", &_);
+                wait = 1;
+                buffer = atoi(instr[1]);
+            }
+
+            i++;
+        }
+    }
+
+    printf("\n%d\n", part1_strength_sum);
+
+}
